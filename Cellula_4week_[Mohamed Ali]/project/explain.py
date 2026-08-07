4969e594861ff49384bb3709dab1fe78bb622ed8
Mohamed-Ali-Elwan <mohamedelwanf650@gmail.com>
2026-08-07 21:18:53 +0300

    Delete Cellula_4week_[Mohamed Ali]/project/explain.py

--- Cellula_4week_[Mohamed Ali]/project/explain.py
+++
@@ -1,9 +0,0 @@
-from llm import LLM
-from IPython.display import  Markdown
-
-class Explain:
-    @staticmethod
-    def explain_code(prompt):
-        llm1 = LLM.get_llm1()
-        response = Markdown(llm1.invoke(prompt).content)
-        return response
